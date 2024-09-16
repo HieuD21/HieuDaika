b8205009a927f059338dceb6e8feb72252d51152
your name <hieu567023@gmail.com>
2024-09-16 08:48:36 +0700

    v1.0.0

--- SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h
+++ SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h
@@ -159,7 +159,7 @@
 #endif /* HSE_STARTUP_TIMEOUT */
 
 #if !defined  (HSI_VALUE)
-  #define HSI_VALUE    ((uint32_t)16000000) /*!< Value of the Internal oscillator in Hz*/
+  #define HSI_VALUE    ((uint32_t)16000000) /*!< Value of the Internal oscillator in sdsdsdsdsdsdsd*/
 #endif /* HSI_VALUE */
 
 /**
